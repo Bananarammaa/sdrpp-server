c47e8a8b285e58b2505854cb3b3fb39110e230bb
dgadams <dgadams.nm@gmail.com>
2025-03-14 17:20:57 -0600

    Update sdrpp-muntz.sh

--- files/sdrpp-muntz.sh
+++ files/sdrpp-muntz.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-# Removes unneeded files.  Use option extglob to allow rm !()
+# This script uses a feature of bash - extglob which allows rm !(exception_list) to
+# selectively remove any file in the current image layer except those listed.  
+# Used for muntzing libraries and files down to the minimal set needed to run the application.
+
 shopt -s extglob
 
 #   Remove select libraries
-cd /usr/lib
-rm -rf !(x86_64-linux-gnu|sdrpp)
+	cd /usr/lib
+	rm -rf !(x86_64-linux-gnu|sdrpp)
 
-# remove all libraries except ...
-cd /usr/lib/x86_64-linux-gnu
-EXC="!(libc.*|ld-linux*"							# basic c library
-#EXC+="|libtinfo*"									# needed for bash
-EXC+="|libselinux*|libacl.*|libattr.*|libpcre*"		# needed for cp
-EXC+="|libresolv.*"									# needed for busybox
-EXC+=")"
-rm -fr $EXC
+# 	remove all libraries except ...
+	cd /usr/lib/x86_64-linux-gnu
+	EXC="!(libc.*|ld-linux*"							# basic c library
+#	EXC+="|libtinfo*"									# needed for bash
+	EXC+="|libselinux*|libacl.*|libattr.*|libpcre*"		# needed for cp
+	EXC+="|libresolv.*"									# needed for busybox
+	EXC+=")"
+	rm -fr $EXC
 
 #   Now copy in needed libraries
-while read p; do
-    cp -a /sdrpp/tmp/$p /usr/lib/x86_64-linux-gnu
-done <<ENDLIST
+	while read p; do
+		cp -a /sdrpp/tmp/$p /usr/lib/x86_64-linux-gnu
+	done <<ENDLIST
     libsdrplay_api.so.*
     libsdrpp_core.so
     libglfw.*
@@ -47,13 +50,13 @@ done <<ENDLIST
 ENDLIST
 
 #   Nuke anything not needed in the container
-rm -rf /var/lib/dpkg
-rm -rf /var/lib/apt
-rm -rf /var/cache/debconf
-rm -rf /var/cache/apt
-rm -rf /usr/share
+	rm -rf /var/lib/dpkg
+	rm -rf /var/lib/apt
+	rm -rf /var/cache/debconf
+	rm -rf /var/cache/apt
+	rm -rf /usr/share
 
-# And last remove everything from sbin and bin except busybox.
-cd /usr/sbin && rm !(nothing)
-cd /usr/bin  && rm !(busybox)
+# 	And last remove everything from sbin and bin except busybox.
+	cd /usr/sbin && rm !(nothing)
+	cd /usr/bin  && rm !(busybox)
 
